de83b7342037132a4a319e049eb20b2c385b0ff5
Luis Guillermo Mora Rivas <lmorar00@hotmail.com>
2021-09-04 11:47:16 -0600

    Initial Commit

---
+++ swuitransitions/swuitransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swuitransitionsApp.swift
+//  swuitransitions
+//
+//  Created by Luis Mora Rivas on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct swuitransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
